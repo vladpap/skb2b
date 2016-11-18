1ab0a15009ebe152f994d6948a921c6a7391d9d9
Vladimir Papin <vladimir.papin@gmail.com>
2016-11-18 03:15:52 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -5,10 +5,32 @@ const app = express();
 app.use(cors());
 app.get('/', (req, res) => {
   res.json({
-    hello: 'JS World',
-  });
+  hello: 'JS World',
 });
+});
+
+app.get('/task2b', (req, res) => {
+  const fullname = req.query.fullname;
+var result = "Invalid fullname";
+if (/[0123456789]/.test(fullname)) {
+  return res.send(result);
+}
+const fullNameArray = fullname.split(" ");
+if (((fullNameArray.length < 4) && (fullNameArray.length > 1)) ||
+((fullNameArray.length == 1) && (fullNameArray[0].length != 0))){
+  var count = 0;
+  result = fullNameArray[fullNameArray.length - 1];
+  while (count < fullNameArray.length - 1) {
+    result += (" " + fullNameArray[count].substr(0, 1) + ".");
+    count ++;
+  }
+
+}
+res.send(result);
+})
+;
 
 app.listen(3000, () => {
   console.log('Your app listening on port 3000!');
-});
+})
+;
